38969dfaa983712de1c8337028d012e8ed50785b
ChrisCruze <cruzc09@gmail.com>
2020-03-15 10:38:15 -0400

    fixed loading animated icon on welcome page

--- Components/Atoms/Animations.js
+++ Components/Atoms/Animations.js
@@ -4,18 +4,29 @@ import { Animated, StyleSheet, Easing, Platform } from "react-native";
 
 export const simpleInterpolation = (start, finish) => ({
   inputRange: [0, 1],
-  outputRange: [start, finish]
+  outputRange: [start, finish],
 });
 
 export const directInverseInterpolation = () => simpleInterpolation(1, 0);
 export const directInterpolation = () => simpleInterpolation(0, 1);
 
-export const AnimatedView = ({ children, style, animations }) => {
+export const AnimatedView = ({ children, style, animations, delay, easing, duration }) => {
   const [animation, updateAnimation] = useState(new Animated.Value(0));
   const styleFlattened = StyleSheet.flatten(style) || {};
   const newStyle = _.pickBy(styleFlattened, (value, key) => key !== "transform");
   const animatedStyle = {};
   const transformStyle = { transform: [] };
+
+  useEffect(() => {
+    Animated.timing(animation, {
+      toValue: 1,
+      duration,
+      delay,
+      easing,
+      useNativeDriver: Platform.OS === "ios",
+    }).start();
+  }, []);
+
   _.forEach(animations, (interpolation, prop) => {
     if (prop !== "transform") {
       animatedStyle[prop] = animation.interpolate(interpolation);
@@ -32,5 +43,7 @@ export const AnimatedView = ({ children, style, animations }) => {
     transformStyle.transform = [...transformStyle.transform, ...styleFlattened.transform];
   }
 
-  return <Animated.View style={[newStyle, animatedStyle, transformStyle]}>{children}</Animated.View>;
+  return (
+    <Animated.View style={[newStyle, animatedStyle, transformStyle]}>{children}</Animated.View>
+  );
 };
