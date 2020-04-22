7dd6d0d2c1df5a67dda263fd17a02dd8e502a853
ChrisCruze <cruzc09@gmail.com>
2020-04-21 22:53:27 -0400

    successfully implemented signup process

--- Components/Pages/SignUpPassword.js
+++ Components/Pages/SignUpPassword.js
@@ -1,21 +1,55 @@
 import React, { useState } from "react";
-import { TextInput } from "react-native";
-import { StyleSheet, View } from "react-native";
 
 import SignUpContainer from "../Molecules/SignUpContainer";
 import { TextField } from "../Atoms/Fields";
-import Theme from "../Atoms/Theme";
-import Text from "../Atoms/Text";
-import Switch from "../Atoms/Switch";
+
+import { AsyncStorage } from "react-native";
+import { SIGNUP_URI } from "react-native-dotenv";
+
+const storeToken = async ({ token }) => {
+  try {
+    console.log({ token });
+    await AsyncStorage.setItem("token", token);
+  } catch (error) {
+    console.log({ error });
+  }
+};
+
+function fetch_authenticate({ email, password, userName, navigation }) {
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({ email: email, password: password, userName: userName });
+
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  fetch(SIGNUP_URI, requestOptions)
+    .then(response => {
+      return response.text();
+    })
+    .then(result => {
+      const token = JSON.parse(result)["token"];
+      return token;
+    })
+    .then(token => {
+      storeToken({ token });
+      navigation.navigate("Home");
+    })
+    .catch(error => console.log("error", error));
+}
 
 const SignUpPassword = ({ navigation }) => {
   const [password, updatePassword] = useState("");
 
   const onSubmit = () => {
     const email = navigation.getParam("email");
-    const username = navigation.getParam("username");
-    console.log({ email, username, password });
-    navigation.navigate("Home");
+    const userName = navigation.getParam("username");
+    fetch_authenticate({ email, password, userName, navigation });
   };
 
   return (
