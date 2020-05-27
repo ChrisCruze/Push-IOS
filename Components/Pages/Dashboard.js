d4691ccade41cf04aab663c70c4b7e8ac6054c15
SeanP1tt <smp323@cornell.edu>
2020-05-27 15:08:18 -0400

    testing dashbard changes

--- Components/Pages/Dashboard.js
+++ Components/Pages/Dashboard.js
@@ -18,6 +18,15 @@ import { AsyncStorage } from "react-native";
 import { DataFlattenConvertGoals } from "../Atoms/BarChart.functions";
 import DashboardTimeStamps from "../Molecules/DashboardTimeStamps";
 
+import {
+  LineChart,
+  // BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
+
 import { useGoalsPull } from "../../API";
 const Dashboard = ({ navigation }) => {
   const logout = () => {
@@ -45,7 +54,37 @@ const Dashboard = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <Header title={"Dashboard"} sub_title={"Today"} logout={logout} />
-      <BarChart chartData={goals_count_by_day_array} />
+      {/* <BarChart chartData={goals_count_by_day_array} /> */}
+      <LineChart
+      data={{
+        labels: goals_count_by_day_array.map(data => data.date),
+        datasets: [
+          {
+            data: goals_count_by_day_array.map(data => data.count)
+          }
+        ]
+      }}
+      width={400}
+      height={256}
+      verticalLabelRotation={30}
+      chartConfig={{
+        backgroundColor: "#e26a00",
+        backgroundGradientFrom: "#fb8c00",
+        backgroundGradientTo: "#ffa726",
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+          borderRadius: 16
+        },
+        propsForDots: {
+          r: "6",
+          strokeWidth: "2",
+          stroke: "#ffa726"
+        }
+      }}
+      bezier
+    >{console.log(goals_count_by_day_array)}</LineChart>
       <TableGrid list_of_lists={goals_count_by_day_array_chunked} />
       <DashboardTimeStamps timeStamps={timeStamps} navigation={navigation} />
     </View>
