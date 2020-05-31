c318d754c5c74f4b187c4aba956dd51d1a1f0979
Anand Angalig <anandangalig@gmail.com>
2020-05-31 17:05:44 -0400

    swap specific colors to confetti cannon

--- Components/Molecules/Confetti.js
+++ Components/Molecules/Confetti.js
@@ -6,12 +6,13 @@ const windowWidth = Dimensions.get("window").width;
 const Confetti = React.forwardRef(function passRefToChild(props, ref) {
   return (
     <ConfettiCannon
-      count={200}
+      count={300}
       origin={{ x: windowWidth / 2, y: -15 }}
       autoStart={false}
       ref={ref}
       fadeOut={true}
       fallSpeed={5000}
+      colors={["#FFD700", "#005AFF", "#000000", "#FFFFFF", "#EBECF0", "#FFDF00"]}
     />
   );
 });
