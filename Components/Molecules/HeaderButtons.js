87ab2376dfb00cb7a041f457dae1eb95cf93d4ca
ChrisCruze <cruzc09@gmail.com>
2020-06-07 15:29:59 -0400

    tried to add drawer open

--- Components/Molecules/HeaderButtons.js
+++ Components/Molecules/HeaderButtons.js
@@ -8,6 +8,7 @@ import {
   SafeAreaView,
   TouchableOpacity,
   TouchableWithoutFeedback,
+  ScrollView,
   Dimensions,
 } from "react-native";
 import { Feather as Icon } from "@expo/vector-icons";
@@ -24,10 +25,10 @@ import {
   Body,
   Item,
   Input,
-  Drawer,
   Card,
   CardItem,
 } from "native-base";
+import Drawer from "react-native-drawer";
 
 const DrawerContainer = ({ children, drawerState, updateDrawerState, content }) => {
   return (
@@ -45,54 +46,110 @@ const DrawerContainer = ({ children, drawerState, updateDrawerState, content }) 
 
 const Sidebar = ({ Code, Name, Note }) => {
   return (
-    <Content>
+    <View style={[styles.drawerInner]}>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          alert("clsoe");
+        }}
+      >
+        <Text>Close</Text>
+      </TouchableWithoutFeedback>
       <Card style={{ flex: 0 }}>
         <CardItem>
           <Body>
-            <Text>{Name || ""}</Text>
-            <Text>{Note || ""}</Text>
-            <Text>{Code || ""}</Text>
+            <Text>{"test"}</Text>
+            <Text>{"test"}</Text>
+            <Text>{"test"}</Text>
           </Body>
         </CardItem>
       </Card>
-    </Content>
+    </View>
   );
 };
-
+const ControlPanel = () => {
+  return (
+    <View style={[styles.container]}>
+      <ScrollView style={styles.container}>
+        <Text style={styles.controlText}>Control Panel</Text>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            alert("hi");
+          }}
+        >
+          <Text>Close Drawer</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  );
+};
 const HeaderButtons = ({ navigation }) => {
   const [drawerState, updateDrawerState] = useState();
-  const [drawerObject, updateDrawerObject] = useState();
 
   return (
-    <Fragment>
+    <View style={[]}>
       <SafeAreaView style={[styles.innerHeader]}>
         <TouchableWithoutFeedback
           onPress={() => {
-            alert("open");
-            drawerState._root.open();
+            drawerState.open();
           }}
         >
           <Icon name={"list"} size={25} {...{ color: "white" }} />
         </TouchableWithoutFeedback>
         <TouchableWithoutFeedback
           onPress={() => {
+            // drawerState.close();
+
             navigation.navigate("Notifications");
           }}
         >
           <Icon name={"bell"} size={25} {...{ color: "white" }} />
         </TouchableWithoutFeedback>
       </SafeAreaView>
+
+      <Drawer
+        content={<ControlPanel />}
+        tapToClose={true}
+        type="overlay"
+        styles={drawerStyles}
+        onClose={() => drawerState.close()}
+        // openDrawerOffset={200}
+        // tweenHandler={Drawer.tweenPresets.parallax}
+        ref={ref => {
+          updateDrawerState(ref);
+        }}
+      ></Drawer>
+
+      {/* 
       <DrawerContainer
-        content={<Sidebar {...drawerObject} />}
+        content={<Sidebar />}
         drawerState={drawerState}
         updateDrawerState={updateDrawerState}
       >
-        <Content></Content>
-      </DrawerContainer>
-    </Fragment>
+        <View>
+          <Text>test</Text>
+        </View>
+      </DrawerContainer> */}
+    </View>
   );
 };
+
 const styles = StyleSheet.create({
+  drawerInner: { flex: 1, height: 1000 },
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "black",
+  },
+  controlText: {
+    color: "white",
+  },
+  button: {
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 10,
+  },
   innerHeader: {
     marginHorizontal: Theme.spacing.base,
     flexDirection: "row",
@@ -102,5 +159,8 @@ const styles = StyleSheet.create({
     marginBottom: 0,
   },
 });
-
+const drawerStyles = {
+  drawer: { shadowColor: "#000000", shadowOpacity: 0.8, shadowRadius: 3 },
+  main: { paddingLeft: 3 },
+};
 export default HeaderButtons;
