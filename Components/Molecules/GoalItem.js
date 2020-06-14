5458b9584c5ec92b90f5be505d2eb69fc76fa686
ChrisCruze <cruzc09@gmail.com>
2020-06-14 12:01:59 -0400

    added local state for improved performance on push

--- Components/Molecules/GoalItem.js
+++ Components/Molecules/GoalItem.js
@@ -81,7 +81,6 @@ const GoalItem = ({
       },
     });
     Vibration.vibrate();
-    refetch();
     goalsListConfetti();
   };
   const deleteGoalPress = () => {
--- Components/Pages/Goals.js
+++ Components/Pages/Goals.js
@@ -16,7 +16,7 @@ import Constants from "expo-constants";
 import { Feather as Icon } from "@expo/vector-icons";
 import Theme from "../Atoms/Theme";
 import GoalItem from "../Molecules/GoalItem";
-import { useGoalsPull, useGoalUpdate, useGoalDelete } from "../../API";
+import { useGoalsPull, useGoalUpdate, useGoalDelete, useGoalState } from "../../API";
 import { AsyncStorage } from "react-native";
 import { NavigationEvents } from "react-navigation";
 import AnimatedHeader from "../Molecules/AnimatedHeader";
@@ -46,13 +46,14 @@ const Goals = ({ navigation }) => {
       .then(() => navigation.navigate("Login"));
   };
   const [scrollAnimation] = React.useState(new Animated.Value(0));
-  const { goals, refetch, loading, networkStatus } = useGoalsPull();
+  const { goals, refetch, loading, networkStatus, updateGoal } = useGoalState();
   NetworkCheckNav({ networkStatus, navigation });
   const { filtered_goals, updateFilter, filter } = GoalsFilter({ goals });
   const { sorted_goals, updateSortOrder, sortOrder } = GoalsSort({ goals: filtered_goals });
 
-  const { updateGoal } = useGoalUpdate();
+  // const { updateGoal } = useGoalUpdate();
   const { removeGoal } = useGoalDelete();
+
   const [notification, setNotification] = useState({});
   const [expoPushToken, setExpoPushToken] = useState("");
 
