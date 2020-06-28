796401ac82685ca5dc0e27a5a6760371e1df01fe
Anand Angalig <anandangalig@gmail.com>
2020-06-28 15:01:23 -0400

    add margin and change color on goal details action buttons

--- Components/Atoms/ButtonNeomorph.js
+++ Components/Atoms/ButtonNeomorph.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, Dimensions, TouchableWithoutFeedback } from "react-native";
 import { Neomorph } from "react-native-neomorph-shadows";
-import Theme from "./Theme";
 
 const ButtonNeomorph = ({ onPress, text }) => {
   return (
@@ -16,21 +15,22 @@ const { width } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   neomorph: {
-    shadowOpacity: 0.4,
+    shadowOpacity: 0.25,
     shadowRadius: 5,
     borderRadius: 30,
     borderColor: "#000000",
     borderWidth: 2,
-    backgroundColor: Theme.palette.background,
-    width: width * 0.3,
-    height: 50,
+    backgroundColor: "#000000",
+    width: width * 0.26,
+    height: 45,
     alignItems: "center",
     justifyContent: "center",
     flex: 1,
   },
   text: {
     fontWeight: "bold",
-    fontSize: 25,
+    fontSize: 20,
+    color: "#FFFFFF",
   },
 });
 export default ButtonNeomorph;
--- Components/Molecules/GoalPageButtons.js
+++ Components/Molecules/GoalPageButtons.js
@@ -51,7 +51,8 @@ const GoalPageButtons = ({
 const styles = StyleSheet.create({
   row_container: {
     flexDirection: "row",
-    justifyContent: "space-around",
+    justifyContent: "space-between",
+    marginHorizontal: 30,
   },
 });
 export default GoalPageButtons;
