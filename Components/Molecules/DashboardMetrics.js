e1e0412fa2e7950d44cc73b36fc748242acf876b
ChrisCruze <cruzc09@gmail.com>
2020-07-22 10:50:53 -0400

    added additional metrics for longest streak and remaining

--- Components/Molecules/DashboardMetrics.js
+++ Components/Molecules/DashboardMetrics.js
@@ -3,6 +3,7 @@ import MetricNeomorph from "../Atoms/MetricNeomorph";
 import { StyleSheet, View } from "react-native";
 import { determinePercentageDone, determineOverDue } from "../Atoms/BarChart.functions";
 import { CarouselMetrics } from "../Atoms/CarouselMetrics";
+import { determineStreak } from "../Atoms/Calculations";
 
 const TotalPushes = ({ timeStamps }) => {
   const total_pushes_count = timeStamps.length;
@@ -24,6 +25,22 @@ const PercentageComplete = ({ goals }) => {
   );
 };
 
+const DueGoals = ({ goals }) => {
+  const remaining_count = goals.filter(function(D) {
+    return determineOverDue({ ...D, goals: array });
+  }).length;
+  return <MetricNeomorph number={remaining_count} text={"Due Goals"} />;
+};
+
+const LongestStreak = ({ goals }) => {
+  const streak_counts = _.map(goals, ({ timeStamps, cadence }) =>
+    determineStreak({ timeStamps, cadence }),
+  );
+  const max_streak_count = _.max(streak_counts);
+
+  return <MetricNeomorph number={max_streak_count} text={"Longest Streak"} />;
+};
+
 const DashboardMetrics = ({ goals, timeStamps }) => {
   return (
     <View style={styles.container}>
@@ -34,8 +51,8 @@ const DashboardMetrics = ({ goals, timeStamps }) => {
           <PercentageComplete goals={goals} />,
           <TotalGoals goals={goals} />,
           <TotalPushes timeStamps={timeStamps} />,
-          <TotalPushes timeStamps={timeStamps} />,
-          <TotalPushes timeStamps={timeStamps} />,
+          <DueGoals goals={goals} />,
+          <LongestStreak goals={LongestStreak} />,
           <TotalPushes timeStamps={timeStamps} />,
         ]}
       />
