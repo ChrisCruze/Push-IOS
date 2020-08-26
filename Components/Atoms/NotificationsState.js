5cf82615f59600f2d5849215c8b6a91a14d6b71b
ChrisCruze <cruzc09@gmail.com>
2020-08-26 19:09:12 -0400

    implemented logic to determine if daily, weekly, or monthly goals are done

--- Components/Atoms/NotificationsState.js
+++ Components/Atoms/NotificationsState.js
@@ -5,6 +5,7 @@ import {
   cadenceTextConvert,
 } from "./Calculations";
 
+import { determinePercentageDone, GoalsFilterCadence } from "./BarChart.functions";
 const determineIfFirstPush = ({ time_stamp_count }) => {
   return time_stamp_count == 1;
 };
@@ -79,6 +80,27 @@ const NotificationTextDetermine = ({ time_stamp_count, streakCount, cadence, max
   }
 };
 
+const NotificationDetermineCadenceComplete = ({ goals }) => {
+  const day_goals = GoalsFilterCadence({ goals, cadence: "daily" });
+  const week_goals = GoalsFilterCadence({ goals, cadence: "weekly" });
+  const month_goals = GoalsFilterCadence({ goals, cadence: "monthly" });
+  const is_day_completed = determinePercentageDone(day_goals) == 100;
+  const is_week_completed = determinePercentageDone(week_goals) == 100;
+  const is_month_completed = determinePercentageDone(month_goals) == 100;
+  const is_all_completed = determinePercentageDone(goals) == 100;
+  if (is_all_completed) {
+    return "You Completed all of your goals!";
+  } else if (is_day_completed) {
+    return "You Completed all of your Daily goals!";
+  } else if (is_week_completed) {
+    return "You Completed all of your Weekly goals!";
+  } else if (is_month_completed) {
+    return "You Completed all of your Monthly goals!";
+  } else {
+    return false;
+  }
+};
+
 const NotificationsStateLogic = ({
   timeStampsWithNew,
   cadence,
@@ -98,7 +120,6 @@ const NotificationsStateLogic = ({
     const pass_dict = { id: _id };
     navigation.navigate("Goal", pass_dict);
   };
-
   const showModal = text => {
     setModalState({
       text,
@@ -110,8 +131,8 @@ const NotificationsStateLogic = ({
       navigateToGoal,
     });
   };
-
-  const text = NotificationTextDetermine({ time_stamp_count, streakCount, cadence, maxPushCount });
+  //const text = NotificationTextDetermine({ time_stamp_count, streakCount, cadence, maxPushCount });
+  const text = NotificationDetermineCadenceComplete({ goals });
   const onPress = () => {
     showModal(text);
   };
@@ -124,6 +145,7 @@ const NotificationsStateLogic = ({
     body: text,
   });
 };
+
 const NotificationsState = ({
   setModalState,
   timeStampsWithNew,
